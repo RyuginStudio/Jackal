7eceb5dc693a731f0b1b09386baeab2848479ba0
vszed <zyrpg@vip.qq.com>
2017-11-09 04:11:16 +0800

    audio souces commit

---
+++ Scripts/CharacterAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAttack : MonoBehaviour {
+
+	public AudioSource machinGunEffect;
+	public AudioSource fireInTheHole;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		attack();
+
+	}
+
+	public void attack()
+	{
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			Debug.Log("machineGun");
+			machinGunEffect.Play();
+		}
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			Debug.Log("FireInTheHole");
+			fireInTheHole.Play();
+		}
+	}
+}
