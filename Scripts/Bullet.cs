d55e2dcc6e0b7291e72b162973eb4b8aef802fe1
vszed <zyrpg@vip.qq.com>
2017-11-11 04:41:03 +0800

    MachinGun's bullet destory

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-	Vector3 bulletInitPos; 
+    public Vector3 bulletInitPos;
+    public GameObject PrefabBullet;
+    public Sprite bulletEffect;  //子弹爆炸效果
 
     // Use this for initialization
     void Start()
     {
-		bulletInitPos = transform.position;
+        bulletInitPos = transform.position;
     }
 
     // Update is called once per frame
@@ -33,15 +35,26 @@ public class Bullet : MonoBehaviour
                 {
                     //Debug.Log("bulletCharacMachinGun");
                     float step = GameData.bulletCharacMachinGunSpeed * Time.deltaTime;
-                    transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(bulletInitPos.x, bulletInitPos.y + GameData.bulletCharacMachinGunDistance), step);
+                    transform.position = Vector2.MoveTowards(transform.position, new Vector2(bulletInitPos.x, bulletInitPos.y + GameData.bulletCharacMachinGunDistance), step);
+
+                    //以下值均需调用C#取得小数点后1位的四舍五入
+                    var initY = System.Math.Round(bulletInitPos.y, 1);
+                    var nowY = System.Math.Round(transform.position.y, 1);
+
+                    if (System.Math.Round(nowY - initY, 1) >= GameData.bulletCharacMachinGunDistance)
+                    {
+                        GetComponent<SpriteRenderer>().sprite = bulletEffect;
+                        Invoke("bulletDestroy", 0.1f);
+                    }
+
                     break;
                 }
-
         }
     }
 
-	void bulletRemove()  //子弹销毁条件：1.碰撞 2.达到射程
-	{
-
-	}
+    void bulletDestroy()  //子弹销毁条件：1.碰撞 2.达到射程
+    {
+        Debug.Log("Destroy prefab");
+        Destroy(PrefabBullet);
+    }
 }
--- Scripts/CharacterAttack.cs
+++ Scripts/CharacterAttack.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterAttack : MonoBehaviour {
+public class CharacterAttack : MonoBehaviour
+{
 
-	public AudioSource machinGunEffect;
-	public AudioSource fireInTheHole;
-	public GameObject prefabBulletMachinGun;
+    public AudioSource machinGunEffect;
+    public AudioSource fireInTheHole;
+    public GameObject prefabBulletMachinGun;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
 
-		attack();
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	public void attack()
-	{
-		if (Input.GetKeyDown(KeyCode.J))
-		{
-			Debug.Log("machineGun");
-			machinGunEffect.Play();
-			Instantiate(prefabBulletMachinGun, transform.position, new Quaternion(0, 0, 0, 0));
-		}
-		if (Input.GetKeyDown(KeyCode.K))
-		{
-			Debug.Log("FireInTheHole");
-			fireInTheHole.Play();
-		}
-	}
+        attack();
+
+    }
+
+    public void attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("machineGun");
+            machinGunEffect.Play();
+            Instantiate(prefabBulletMachinGun, transform.position, new Quaternion(0, 0, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("FireInTheHole");
+            fireInTheHole.Play();
+        }
+    }
 }
