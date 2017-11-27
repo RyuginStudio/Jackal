456a3980dd0050da059e744a7b25391471377863
vszed <zyrpg@vip.qq.com>
2017-11-27 12:50:46 +0800

    joystick

---
+++ Scripts/Joystick.cs
@@ -0,0 +1,28 @@
+﻿//http://www.xuanyusong.com/archives/3924
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Joystick : ScrollRect
+{
+    protected float mRadius = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+        //计算摇杆块的半径
+        mRadius = (transform as RectTransform).sizeDelta.x * 0.5f;
+    }
+
+    public override void OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        var contentPostion = this.content.anchoredPosition;
+        if (contentPostion.magnitude > mRadius)
+        {
+            contentPostion = contentPostion.normalized * mRadius;
+            SetContentAnchoredPosition(contentPostion);
+        }
+    }
+}
